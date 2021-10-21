be49b132d35cdc218a40efa1f5f86802339e8749
unknown <ermolov1337@gmail.com>
2021-10-21 17:10:05 +0300

    финал. реализация drag and drop

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 let order = "ASK";
-const taskList = []; // создаём массив, куда потом будем пушить и сортировать
+let taskList = []; // создаём массив, куда потом будем пушить и сортировать
 
 const container = document.querySelector(".container")
 const buttonSort = document.querySelector(".button-Sort") //получаем доступ к кнопке sort
 const mainInput = document.querySelector(".main-input") //получаем доступ к первому инпуту
 const buttonRound = document.querySelector(".button-round") //получаем доступ к главной кнопке
 const addDiv = document.querySelector(".list-container") //доступ к диву, куда будем добавлять
+const clearTask = document.querySelector(".clear-task") //доступ к очистке
 
+
+
+
+
+clearTask.addEventListener("click", clearInput)
 buttonRound.addEventListener("click", renderTask)
+buttonSort.addEventListener("click", sortTask)
 container.addEventListener("keyup", (event) => {
     console.log(event.key)
     if (event.key === "Enter") {
         renderTask();
     }
 })
-buttonSort.addEventListener("click", sortTask)
 
+function clearInput() {
+    mainInput.value = ""
+}
 
 //рисуем задачи
 function renderTask() {
@@ -32,8 +41,11 @@ function renderTask() {
         const conteiner = document.createElement("div")
         const newInput = document.createElement("input")
         const newButton = document.createElement("button")
-        addDiv.prepend(conteiner)
-        conteiner.classList.add("enter-block")
+      
+        addDiv.append(conteiner)
+        conteiner.draggable = "true";
+        conteiner.classList.add("enter-block-1", "move")
+
         conteiner.append(newInput)
         newInput.value = taskList[index]
         newInput.classList.add("main-input")
@@ -58,15 +70,17 @@ function addTask() {
         }
     } else {
         taskList.push(mainInput.value)
-     }
     }
-
+}
 
 //Удаляем задачу
 function deleteTask(event) {
     const deleteDiv = event.target.parentElement;
     deleteDiv.remove()
-    taskList.pop()
+    const inputValue = deleteDiv.querySelector("input")
+    let gg = taskList.filter(item => item !== inputValue.value)
+    taskList = [...gg]
+    console.log(taskList)
 }
 
 
@@ -93,10 +107,13 @@ function sortTask() {
         const conteiner = document.createElement("div")
         const newInput = document.createElement("input")
         const newButton = document.createElement("button")
-        addDiv.prepend(conteiner)
-        conteiner.classList.add("enter-block")
+
+
+        addDiv.append(conteiner)
+        conteiner.draggable = "true";
+        conteiner.classList.add("enter-block-1", "move")
         conteiner.append(newInput)
-        newInput.value = sort[index]
+        newInput.value = taskList[index]
         newInput.classList.add("main-input")
         conteiner.append(newButton)
         newButton.addEventListener("click", deleteTask)
@@ -120,3 +137,81 @@ function sortBackAlphabet(array) {
     })
     return sort;
 }
+
+
+//Перенос объектов
+
+//const tasksListElement = document.querySelector(`.tasks__list`);
+//Добавляем и удаляем стили с объектов, которые переносим...
+const tasksListElement = document.querySelector('.list-container');
+
+const taskElements = tasksListElement.querySelectorAll('.conteiner');
+
+for (const task of taskElements) {
+    task.draggable = true;
+}
+
+tasksListElement.addEventListener('dragstart', (evt) => {
+    evt.target.classList.add(`selected`);
+
+    const allElements = document.querySelectorAll(".enter-block-1")
+
+    allElements.forEach((item) => {
+        console.log(item)
+        item.classList.remove('enter-block-1')
+        item.classList.add('enter-block-2')
+    })
+
+});
+
+tasksListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+
+
+    const allElements = document.querySelectorAll(".enter-block-2")
+
+    allElements.forEach((item) => {
+        console.log(item)
+        item.classList.remove('enter-block-2')
+        item.classList.add('enter-block-1')
+    })
+
+
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
+
+    return nextElement;
+};
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
+
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+    const isMoveable = activeElement !== currentElement
+    if (!isMoveable) {
+        return;
+    }
+
+    const nextElement = getNextElement(evt.clientY, currentElement);
+
+    if (
+        nextElement &&
+        activeElement === nextElement.previousElementSibling ||
+        activeElement === nextElement
+    ) {
+        return;
+    }
+
+    tasksListElement.insertBefore(activeElement, nextElement);
+});
+
+
+
